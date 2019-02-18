e3140c32f9aa41c60869322324bbd76a6715499c
“Zeyad” <zeyad888@outlook.com>
2019-02-18 14:53:54 +0300

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Zeyad AlHusainan on 18/02/2019.
+//  Copyright © 2019 Zeyad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
